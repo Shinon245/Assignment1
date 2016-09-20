2674a717711aff14c7a3db991aa68ff55795714d
Cameron Lee Crow <crowcameron4@gmail.com>
2016-09-20 11:30:00 -0600

    Finding max and min

--- src/javaapplication7/JavaApplication7.java
+++ src/javaapplication7/JavaApplication7.java
@@ -19,7 +19,17 @@ public class JavaApplication7
         int i = 0;
         for (i=0;i<str1.length()-sub+1;i++)
         {
-            System.out.println(str1.substring(i,i+sub));
+            String substr = str1.substring(i,i+sub);
+            if (i == 0)
+            {
+                maxstr = substr;
+                minstr = substr;
+            }
+            else 
+            {
+                if (substr.compareTo(minstr)<0) minstr = substr;
+                if (substr.compareTo(maxstr)>0) maxstr = substr;
+            }
         }
     }
 
