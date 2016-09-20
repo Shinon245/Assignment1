a72a9d8c5333d4a8ddb492bcaa82f7d20783bcb4
Cameron Lee Crow <crowcameron4@gmail.com>
2016-09-20 11:22:47 -0600

    Substring Finder

---
+++ src/javaapplication7/JavaApplication7.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication7;
+
+/**
+ *
+ * @author Cameron
+ */
+public class JavaApplication7 
+{
+    
+    public static void printout(String str1, int sub)
+    {
+        int i = 0;
+        for (i=0;i<str1.length()-sub+1;i++)
+        {
+            System.out.println(str1.substring(i,i+sub));
+        }
+    }
+
+
+    
+    public static void main(String[] args) 
+    {
+        printout("Hello", 2);
+    }
+    
+}
